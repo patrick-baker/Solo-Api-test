7bf8d1b63c3698bbf7f46e5d246b07985830e111
Kai Peterson <pete9372@umn.edu>
2019-11-08 10:42:24 -0600

    setup onClick for favoriting images

--- src/components/ShowResults/ShowResults.js
+++ src/components/ShowResults/ShowResults.js
@@ -4,10 +4,17 @@ import {connect} from 'react-redux'
 
 class ShowResults extends Component {
 
+  favoriteImage = (url) => {
+    // dispatch to POST saga here
+  }
+
   render() {
     return (
       <div>
-          {this.props.reduxState.APIReducer.map(gif => <img src={gif.url}/>)}
+          {this.props.reduxState.APIReducer.map( (gif) => 
+            <img src={gif.url}/>
+            <button onClick={() => this.favoriteImage(gif.url)}>Favorite</button>
+          )}
       </div>
     );
   }
